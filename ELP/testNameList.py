03205b665e1f30b6c1123efd42301a33c963c0ce
baoer <2675835744@qq.com>
2018-08-28 01:16:40 +0800

    ELP

---
+++ ELP/testNameList.py
@@ -0,0 +1,39 @@
+from django.http import HttpResponse
+import pymysql
+import json
+
+def getDao():
+    config = {
+        'host': '39.105.96.239',
+        'port': 3306,
+        'user': 'root',
+        'password': '662662',
+        'db': 'ELP',
+        'charset': 'utf8mb4',
+        'cursorclass': pymysql.cursors.DictCursor,
+    }
+    return pymysql.connect(**config)
+
+def getData(db, sql):
+    try:
+        cursor = db.cursor()
+        cursor.execute(sql)
+        data = cursor.fetchall()
+    except Exception as e:
+        print(e)
+        return 'HAHA'
+    finally:
+        db.close()
+        cursor.close()
+    return data
+
+
+def getTestNameList(requset):
+    if requset.GET['gradeType'] == 'A':
+        sql = 'select * from test_a'
+        data = getData(getDao(), sql)
+        print(data)
+        return HttpResponse(json.dumps(data))
+    else:
+        sql = 'select * from test_b'
+        return HttpResponse(json.dumps(getData(getDao(), sql)))
